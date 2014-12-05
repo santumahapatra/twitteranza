200b8d7a0ab04c5f3edcf7af5f9f3be1a6f1f2c6
Santu Mahapatra <santu.m@multunus.com>
2014-12-05 16:30:56 +0530

    Adding index page for the Tweets controller

--- spec/controllers/tweets_controller_spec.rb
+++ spec/controllers/tweets_controller_spec.rb
@@ -9,4 +9,11 @@ RSpec.describe TweetsController, :type => :controller do
     end
   end
 
+  describe "#index" do
+    it "returns http success" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+  end
+
 end
