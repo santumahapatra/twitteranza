c5f057238e4d982dbd503023b28a95e352931040
Santu Mahapatra <santu.m@multunus.com>
2014-12-05 13:15:37 +0530

    User can now Tweet

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,30 @@
+class TweetsController < ApplicationController
+
+  def index
+    @tweets = Tweet.all
+  end
+
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    @tweet = Tweet.new(tweet_params)
+
+    respond_to do |format|
+      if @tweet.save
+        format.html { redirect_to tweets_path, notice: 'Tweet was successfully created.' }
+        format.json { render :show, status: :created, location: @tweet }
+      else
+        format.html { render :new }
+        format.json { render json: @tweet.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+    def tweet_params
+      params.require(:tweet).permit(:content)
+    end
+end
---
+++ spec/controllers/tweets_controller_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe TweetsController, :type => :controller do
+
+  describe "#new" do
+    it "returns http success" do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
