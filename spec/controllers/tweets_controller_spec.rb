a5230988d3d424d74e8feecbb06fabb01e2154d8
Santu Mahapatra <santu.m@multunus.com>
2014-12-09 16:57:35 +0530

    Updating the tests for Tweets controller

--- spec/controllers/tweets_controller_spec.rb
+++ spec/controllers/tweets_controller_spec.rb
@@ -14,6 +14,15 @@ RSpec.describe TweetsController, :type => :controller do
     it 'renders index template' do
       expect(response).to render_template(:index)
     end
+
+    it 'has instance variable tweet' do
+      expect(assigns(:tweet)).to be_a_new(Tweet)
+    end
+
+    it 'has instance variable tweets' do
+      all_tweets = Tweet.all
+      expect(assigns(:tweets)).to eq(all_tweets)
+    end
   end
 
   describe 'POST #create' do
@@ -21,7 +30,7 @@ RSpec.describe TweetsController, :type => :controller do
     context 'if tweet is blank' do
       it 'displays the page with error message' do
         post :create, tweet: {content: '' }
-        expect(flash[:notice]).to match(/^Tweet is blank or greater than 140 characters/)
+        expect(flash[:notice]).to match(/^Tweet cannot be created if content is blank or greater than 140 characters/)
       end
     end
 
@@ -29,7 +38,7 @@ RSpec.describe TweetsController, :type => :controller do
       it 'displays the page with error message' do
         long_tweet = 'a' * 141
         post :create, tweet: { content: long_tweet }
-        expect(flash[:notice]).to match(/^Tweet is blank or greater than 140 characters/)
+        expect(flash[:notice]).to match(/^Tweet cannot be created if content is blank or greater than 140 characters/)
       end
     end
 
