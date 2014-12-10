e8f049d4c0124ced66dcc6f4ddbc81e52ad77a38
Santu Mahapatra <santu.m@multunus.com>
2014-12-10 16:37:50 +0530

    Implementing follow feature

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,10 @@ Rails.application.routes.draw do
   resources :users do
     member do
       get :tweets
-      #get :following, :followers
+      get :following, :followers
     end
   end
+  resources :relationships, only: [:create, :destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
